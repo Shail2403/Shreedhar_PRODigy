1bec3ec073c9f91def48903fffce34142266586f
Shail2403 <shahshail2405@gmail.com>
2026-03-08 23:39:35 +0530

    Fix build crash by using os.getenv with defaults for all sensitive keys

--- backend/shridhar_enterprise/settings.py
+++ backend/shridhar_enterprise/settings.py
@@ -169,8 +169,8 @@ CORS_ALLOW_CREDENTIALS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', config('EMAIL_HOST_USER', default=''))
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', config('EMAIL_HOST_PASSWORD', default=''))
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Shridhar Enterprise <gcp.shridharenterprise@gmail.com>')
 
@@ -216,12 +216,12 @@ STORAGES = {
 # ---------------------------------------------------------------------------
 # Google Maps API
 # ---------------------------------------------------------------------------
-GOOGLE_MAPS_API_KEY = config('GCP_MAPS_PLATFORM_API')
+GOOGLE_MAPS_API_KEY = os.getenv('GCP_MAPS_PLATFORM_API', config('GCP_MAPS_PLATFORM_API', default=''))
 
 # ---------------------------------------------------------------------------
 # PayPal
 # ---------------------------------------------------------------------------
-PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
+PAYPAL_CLIENT_ID = os.getenv('PAYPAL_CLIENT_ID', config('PAYPAL_CLIENT_ID', default=''))
 PAYPAL_CLIENT_SECRET = config('PAYPAL_CLIENT_SECRET', default='')
 PAYPAL_MODE = config('PAYPAL_MODE', default='sandbox')
 
